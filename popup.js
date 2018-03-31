db2b1da3bb9c6682083fe00911e6fb6df3bcb96c
Julian <juliandduque@hotmail.com>
2018-03-30 22:52:46 -0400

    base model

--- popup.js
+++ popup.js
@@ -1,21 +1,60 @@
+var API = "https://api.clarifai.com/v2/models/bd367be194cf45149e75f01d59f77ba7/outputs";
+var APIKEY = "b6130538acd741cb8402552ba042654f";
+
+
 document.addEventListener('DOMContentLoaded', function () {
     var checkPageButton = document.getElementById('checkPage');
 
     checkPageButton.addEventListener('click', function () {
 
-        chrome.tabs.getSelected(null, function (tab) {
-            d = document;
+        chrome.tabs.captureVisibleTab(null, {}, function (image)
+        {
 
-            var f = d.createElement('form');
-            f.action = 'http://gtmetrix.com/analyze.html?bm';
-            f.method = 'post';
-            var i = d.createElement('input');
-            i.type = 'hidden';
-            i.name = 'url';
-            i.value = tab.url;
-            f.appendChild(i);
-            d.body.appendChild(f);
-            f.submit();
+            chrome.extension.getBackgroundPage().console.log(image);
+
+            // Setup the JSON payload to send to the API
+            /*var jsonPayload =
+                {
+                    "inputs":
+                    [
+                        "data": 
+                        {
+                            "image":image
+                        }
+                    ]
+                };
+
+            Console.log(jsonPayload);
+
+            //jsonPayload = JSON.stringify(jsonPayload);
+           
+            // Setup the HMLHttpRequest
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", API, true);
+            xhr.setRequestHeader("Content-Authorization", "application/json");
+            xhr.setRequestHeader("Content-type", APIKEY);
+
+            // Attempt to call clarifai
+            try
+            {
+                // Send the XMLHttpRequest
+                //xhr.send(jsonPayload);
+
+                // Parse the JSON returned from the request
+                var jsonObject = JSON.parse(xhr.responseText);
+
+                // If the returned JSON contains an error then set the HTML login result message
+                if (jsonObject.error || !jsonObject.success) {
+                    document.getElementById("result").innerHTML = jsonObject.error;
+                    return false;
+                }
+                
+            } catch (e)
+            {
+                // If there is an error parsing the JSON, attempt to set the HTML login result message
+                document.getElementById("result").innerHTML = e.message;
+            }
         });
+            */
     }, false);
 }, false);
